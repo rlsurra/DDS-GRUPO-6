f301829e24af354c315e140a9b3209bfeac884a5
Juan Martin Conde <juanma.conde@hotmail.com>
2019-04-28 15:54:02 -0300

    Se agrega Tipo de Prenda y Categoria:

---
+++ src/test/java/ar/edu/utn/frba/dds/TipoPrendaTest.java
@@ -0,0 +1,14 @@
+package ar.edu.utn.frba.dds;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TipoPrendaTest {
+
+    @Test
+    public void crearPrendaDeUnaCategoriaTest() {
+        //CREAR UNA REMERA DEL TIPO CORTA Y CATEGORIA SUPEROR, QUE DEVUELVA ESO
+        Assert.assertEquals(new HelloWorld().saludar(), "¡Hola Mundo!");
+    }
+
+}
