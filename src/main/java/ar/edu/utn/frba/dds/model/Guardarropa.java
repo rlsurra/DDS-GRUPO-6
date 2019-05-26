3ca71d4e0bedb7b4faad83d46b8b26cab79f8bd1
Diego Zuleta <diegorzuleta@gmail.com>
2019-05-26 10:43:56 -0300

    Se diferencias las prendas en guardaropa por tipo

--- src/main/java/ar/edu/utn/frba/dds/model/Guardarropa.java
+++ src/main/java/ar/edu/utn/frba/dds/model/Guardarropa.java
@@ -1,26 +1,57 @@
 package ar.edu.utn.frba.dds.model;
 
+import ar.edu.utn.frba.dds.model.prenda.TipoPrendaAccesorio;
+import ar.edu.utn.frba.dds.model.prenda.TipoPrendaCalzado;
+import ar.edu.utn.frba.dds.model.prenda.TipoPrendaInferior;
+import ar.edu.utn.frba.dds.model.prenda.TipoPrendaSuperior;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Guardarropa {
 
-    private List<Prenda> prendas = new ArrayList<>();
+    private List<TipoPrendaSuperior> prendasSuperiores = new ArrayList<>();
+    private List<TipoPrendaInferior> prendasInferiores = new ArrayList<>();
+    private List<TipoPrendaCalzado> prendasCalzado = new ArrayList<>();
+    private List<TipoPrendaAccesorio> prendasAccesorio = new ArrayList<>();
+
     private GeneradorDeAtuendos generadorDeAtuendos = GeneradorDeAtuendos.getInstance();
 
-    public Guardarropa(){
-
+    /*
+    getters
+     */
+    public List<TipoPrendaSuperior> getPrendasSuperiores() {
+        return prendasSuperiores;
     }
 
-    public List<Prenda> getPrendas() {
-        return prendas;
+    public List<TipoPrendaInferior> getPrendasInferiores() {
+        return prendasInferiores;
     }
 
-    public void setPrendas(List<Prenda> prendas) {
-        this.prendas = prendas;
+    public List<TipoPrendaCalzado> getPrendasCalzado() {
+        return prendasCalzado;
     }
 
-    public List<Atuendo> getAtuendos() {
-        return generadorDeAtuendos.generarAtuendos(prendas);
+    public List<TipoPrendaAccesorio> getPrendasAccesorio() {
+        return prendasAccesorio;
+    }
+
+    /*
+    setters
+     */
+    public void setPrendasSuperiores(List<TipoPrendaSuperior> prendasSuperiores) {
+        this.prendasSuperiores = prendasSuperiores;
+    }
+
+    public void setPrendasInferiores(List<TipoPrendaInferior> prendasInferiores) {
+        this.prendasInferiores = prendasInferiores;
+    }
+
+    public void setPrendasCalzado(List<TipoPrendaCalzado> prendasCalzado) {
+        this.prendasCalzado = prendasCalzado;
+    }
+
+    public void setPrendasAccesorio(List<TipoPrendaAccesorio> prendasAccesorio) {
+        this.prendasAccesorio = prendasAccesorio;
     }
 }
