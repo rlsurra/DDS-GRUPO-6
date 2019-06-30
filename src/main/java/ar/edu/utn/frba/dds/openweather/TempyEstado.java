9780a859317a0db64b892cef04700bddb4ce1482
diegozurita <diego2708@hotmail.com.ar>
2019-06-30 20:34:42 -0300

    primer api clima openweather

---
+++ src/main/java/ar/edu/utn/frba/dds/openweather/TempyEstado.java
@@ -0,0 +1,4 @@
+package ar.edu.utn.frba.dds.openweather;
+
+public class TempyEstado {
+}
--- src/main/java/ar/edu/utn/frba/dds/services/ResponseClima.java
+++
@@ -1,7 +0,0 @@
-package ar.edu.utn.frba.dds.services;
-
-import ar.edu.utn.frba.dds.model.prenda.Clima;
-
-public class ResponseClima {
-    public Clima data;
-}
