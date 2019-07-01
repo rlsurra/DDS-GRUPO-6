4a57b4607f5e5fc9e013e44c2515e3bbdf4b34be
diegozurita <diego2708@hotmail.com.ar>
2019-06-30 21:00:54 -0300

    api clima cargo pom

--- src/main/java/ar/edu/utn/frba/dds/openweather/Temperatura.java
+++ src/main/java/ar/edu/utn/frba/dds/openweather/Temperatura.java
@@ -1,4 +1,16 @@
 package ar.edu.utn.frba.dds.openweather;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Temperatura {
+    float temp;
+
+    public float getTemp() {
+        return temp;
+    }
+
+    public void setTemp(float temp) {
+        this.temp = temp;
+    }
 }
+
--- src/test/java/ar/edu/utn/frba/dds/OpenweatherTest.java
+++ src/test/java/ar/edu/utn/frba/dds/OpenweatherTest.java
@@ -1,4 +1,15 @@
 package ar.edu.utn.frba.dds;
 
+import ar.edu.utn.frba.dds.openweather.TempyEstado;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class OpenweatherTest {
+
+   @Test
+    public void testRequestTemperatura(){
+       TempyEstado temperatura = TempyEstado.tempPorIdCiudad(3435910);
+       Assert.assertNotNull(temperatura.toString());
+       System.out.println(temperatura.toString());
+   }
 }
