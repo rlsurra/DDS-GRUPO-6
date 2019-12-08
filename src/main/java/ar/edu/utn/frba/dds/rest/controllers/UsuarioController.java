f9be1551dcaa11769265d4a7086795834b64725b
Walter <sebastian.taka@hotmail.com>
2019-12-08 13:24:34 -0300

    Agrego para eliminar usuario

--- src/main/java/ar/edu/utn/frba/dds/rest/controllers/UsuarioController.java
+++ src/main/java/ar/edu/utn/frba/dds/rest/controllers/UsuarioController.java
@@ -1,13 +1,15 @@
 package ar.edu.utn.frba.dds.rest.controllers;
 
+import ar.edu.utn.frba.dds.Autenticacion.Autenticacion;
+import ar.edu.utn.frba.dds.Autenticacion.Session;
+import ar.edu.utn.frba.dds.exceptions.EntidadNoEncontradaException;
+import ar.edu.utn.frba.dds.exceptions.UserNotLoggedException;
+import ar.edu.utn.frba.dds.model.guardarropa.Guardarropa;
 import ar.edu.utn.frba.dds.model.usuario.Usuario;
 import ar.edu.utn.frba.dds.model.usuario.tipoUsuario.TipoUsuario;
 import ar.edu.utn.frba.dds.model.usuario.tipoUsuario.TipoUsuarioPremium;
 import ar.edu.utn.frba.dds.persistence.Repositorio;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -26,4 +28,15 @@ public class UsuarioController {
         return repositorio.getEntidadById(Usuario.class, usuario.getId());
     }
 
+    @DeleteMapping
+    public Usuario deleteOne(@RequestHeader("Authorization") String token) throws EntidadNoEncontradaException, UserNotLoggedException {
+        Repositorio repo = null;
+        Usuario usuario = null;
+        repo = Repositorio.getInstance();
+        Session session = Autenticacion.getSession(token);
+        usuario = repo.getEntidadById(Usuario.class, session.getUsuarioId());
+        repo.delete(usuario);
+        Autenticacion.deleteSession(token);
+        return usuario;
+    }
 }
