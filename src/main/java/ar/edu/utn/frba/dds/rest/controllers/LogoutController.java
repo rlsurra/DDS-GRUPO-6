3cf86ac8445de800429038cac94cb7fb85c6ee03
diego-rz <diegorzuleta@gmail.com>
2019-12-11 20:21:32 -0300

    Agregado filtro cors

--- src/main/java/ar/edu/utn/frba/dds/rest/controllers/LogoutController.java
+++ src/main/java/ar/edu/utn/frba/dds/rest/controllers/LogoutController.java
@@ -4,12 +4,16 @@ import ar.edu.utn.frba.dds.Autenticacion.Autenticacion;
 import ar.edu.utn.frba.dds.Autenticacion.Session;
 import ar.edu.utn.frba.dds.Autenticacion.Sessions;
 import ar.edu.utn.frba.dds.exceptions.UserNotLoggedException;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RequestMapping(path = "logout")
 public class LogoutController {
 
