cfbbc55f89692db5d4039d96ec8f44aec9b27189
Walter <sebastian.taka@hotmail.com>
2019-12-13 23:45:43 -0300

    Agregar Evento

--- src/main/java/ar/edu/utn/frba/dds/model/evento/EventoSimple.java
+++ src/main/java/ar/edu/utn/frba/dds/model/evento/EventoSimple.java
@@ -1,9 +1,14 @@
 package ar.edu.utn.frba.dds.model.evento;
 
+import ar.edu.utn.frba.dds.exceptions.ParametrosInvalidosException;
+import ar.edu.utn.frba.dds.rest.DTOs.EventoDTO;
+import org.apache.tomcat.jni.Local;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity(name = "eventosimple")
 @Table
@@ -26,5 +31,27 @@ public class EventoSimple extends Evento {
 
     public void setFecha(LocalDateTime fecha) {
         this.fecha = fecha;
+    }
+
+    @Override
+    public Evento fillWithDTO(EventoDTO eventoDTO, Evento evento){
+        evento = super.fillWithDTO(eventoDTO, evento);
+        EventoSimple eventoSimple = (EventoSimple) evento;
+        if (eventoDTO.getFecha() == null){
+            throw new ParametrosInvalidosException("Los eventos simples deben tener una fecha de ejecucion");
+        }
+        if (eventoDTO.getHora() == null){
+            throw new ParametrosInvalidosException("Los eventos simples deben tener una hora de ejecucion");
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(eventoDTO.getFecha() + " " + eventoDTO.getHora(), formatter);
+        eventoSimple.setFecha(dateTime);
+        return eventoSimple;
+    }
+
+    public void initOnDemand() {
+        if (this.getFecha().isBefore(LocalDateTime.now().plusDays(4))){
+            this.init();
+        }
     }
 }
